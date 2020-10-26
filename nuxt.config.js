593a4cbd5c66217f5805ccf216865ea844e7692c
nizammarwadi <nizammarwadi94@gmail.com>
2020-10-27 00:41:13 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
   ],
-
+  router: {base: '/beritaku/'},
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
